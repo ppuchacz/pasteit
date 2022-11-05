0c7f11cfaf53911e09667a25c41f6023c534239a
Piotr Puchacz <Piotr.Puchacz@silkycoders.com>
2022-11-05 21:48:06 +0100

    Basic working versin

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from webapp.models import Note
+
 # Register your models here.
+
+admin.site.register(Note)
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Note
+
+
+def index(request):
+    notes = Note.objects.all()
+    context = {
+        'notes': notes
+    }
+    return render(request, 'webapp/index.html', context)
+
+
+def note(request, id=1):
+    note = Note.objects.get(pk=id)
+    context = {
+        'note': note
+    }
+    return render(request, 'webapp/note.html', context)
